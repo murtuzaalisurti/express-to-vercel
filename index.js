f71e3af53bcc2372259a3118d33bab7421b06ab1
hauvm <92607299+hauvm@users.noreply.github.com>
2022-09-21 07:33:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+import * as cheerio from 'cheerio';
+import fetch from 'node-fetch';
 const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+    res.json("Home");
 })
 
 app.listen(process.env.PORT || 3000);
