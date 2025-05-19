03108363ae2f0df12c11aba4f58e52b1e2ac0184
murtuzaalisurti <murtuza.creativity@gmail.com>
2025-05-19 20:11:23 +0530

    fix: make root folder the output folder

--- api/index.js
+++ api/index.js
@@ -2,10 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('public'))
-
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+    res.send("Express App Responded");
 })
 
 app.listen(process.env.PORT || 3000);
